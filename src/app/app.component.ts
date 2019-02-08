4b5f073057f4740c8cd453b3d5f82f0f63e53b0f
owengiri20 <46738862+owengiri20@users.noreply.github.com>
2019-02-08 10:09:49 +0800

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name:string;
+
+  constructor(){
+    this.name = 'Owen'
+    alert('change')
+    this.changeName('name has changed')
+  }
+
+   changeName(name:string):void{
+    this.name = name;
+  }
 }
